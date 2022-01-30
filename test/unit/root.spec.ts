87f970d8badbae831f7e71f08f39356dbc4db4a7
Victor Garcia <victor.garcia@malt.com>
2022-01-30 22:22:58 +0100

    Fixed tests + added

--- test/unit/root.spec.ts
+++ test/unit/root.spec.ts
@@ -17,7 +17,11 @@ describe('Init route file', () => {
   it('should generate to correct tree for the pages folder', async () => {
     const routeFile = await readFile('playground/generated/__routes.ts', 'utf-8');
     const definitionsFile = await readFile('playground/generated/typed-router.d.ts', 'utf-8');
+    const hookFile = await readFile('playground/generated/__useTypedRouter.ts', 'utf-8');
+    const pluginFile = await readFile('playground/plugins/__typed-router.ts', 'utf-8');
     expect(routeFile).toMatchSnapshot();
     expect(definitionsFile).toMatchSnapshot();
+    expect(hookFile).toMatchSnapshot();
+    expect(pluginFile).toMatchSnapshot();
   });
 });
