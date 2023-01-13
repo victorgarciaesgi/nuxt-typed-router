4f2c6b57db5b25f8078e4dddf8fe2f78c260a674
Victor Garcia <victor.garcia@malt.com>
2023-01-13 16:54:15 +0100

    Fixed config

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,5 +1,6 @@
 export default defineAppConfig({
   docus: {
+    url: 'https://nuxt-typed-router.vercel.app/',
     title: 'Nuxt Typed Router',
     description:
       'Provide a type safe router to Nuxt with auto-generated typed definitions for route names and autocompletion for route params',
