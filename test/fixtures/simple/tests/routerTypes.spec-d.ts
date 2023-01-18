3bc98bba25ba2dc6347ad8a8df7044b2a26e935a
Victor Garcia <victor.garcia@malt.com>
2023-01-18 18:24:17 +0100

    added useRoute type tests

--- test/fixtures/simple/tests/routerTypes.spec-d.ts
+++ test/fixtures/simple/tests/routerTypes.spec-d.ts
@@ -2,6 +2,12 @@ import { useRouter } from '@typed-router';
 
 export const router = useRouter();
 
+// @ts-expect-error
+router.resolve({ name: 'index', params: { id: '1' } }); // Error
+
+// @ts-expect-error
+router.resolve({ name: 'blabla-baguette' }); // Error
+
 // ---- [id].vue
 
 // @ts-expect-error
@@ -33,3 +39,21 @@ router.resolve({ name: 'user-slug', params: { slug: 1 } }); // Error
 router.resolve({ name: 'user-slug', params: { slug: ['foo'] } }); // Good
 
 router.resolve({ name: 'user-slug', params: { slug: [1, 2, 3] } }); // Good
+
+// ---- [one]-foo-[two].vue
+
+// @ts-expect-error
+router.resolve({ name: 'user-one-foo-two' }); // Error
+// @ts-expect-error
+router.resolve({ name: 'user-one-foo-two', params: { one: 1 } }); // Error
+
+router.resolve({ name: 'user-one-foo-two', params: { one: 1, two: '2' } }); // Good
+
+// ---- [id]/[slug].vue
+
+// @ts-expect-error
+router.resolve({ name: 'user-id-slug' }); // Error
+// @ts-expect-error
+router.resolve({ name: 'user-id-slug', params: { id: 1 } }); // Error
+
+router.resolve({ name: 'user-id-slug', params: { slug: '2' } }); // Good
