77362eca566c7a126012ee4c3377dffcb5239193
Victor Garcia <victor.garcia@malt.com>
2023-01-18 17:54:25 +0100

    updated timeout

--- test/e2e/base.spec.ts
+++ test/e2e/base.spec.ts
@@ -6,7 +6,7 @@ import { expectNoClientErrors } from './utils';
 describe('Root page', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('../fixtures/simple', import.meta.url)),
-    setupTimeout: 12000,
+    setupTimeout: 120000,
   });
   it('should display the root page without error', async () => {
     const html = await $fetch('/');
