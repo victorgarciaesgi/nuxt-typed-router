ad5b0df4e0e3f74713cafd140c2b011813703cd5
Victor Garcia <victor.garcia@malt.com>
2023-02-09 00:19:39 +0100

    fix types

--- test/fixtures/complex/tests/routerTypes.spec-d.ts
+++ test/fixtures/complex/tests/routerTypes.spec-d.ts
@@ -8,57 +8,52 @@ const { $typedRouter } = useNuxtApp();
 $typedRouter.resolve({ name: 'index', params: { id: 1 } });
 
 // @ts-expect-error
-router.resolve({ name: 'index', params: { id: '1' } }); // Error
-
+router.resolve({ name: 'index', params: { id: '1' } });
 // @ts-expect-error
-router.resolve({ name: 'blabla-baguette' }); // Error
+router.resolve({ name: 'blabla-baguette' });
 
 // ---- [id].vue
 
 // @ts-expect-error
-router.resolve({ name: 'user-id' }); // Error
-
+router.resolve({ name: 'user-id' });
 // @ts-expect-error
-router.resolve({ name: 'user-id', params: { foo: 'bar' } }); // Error
-
+router.resolve({ name: 'user-id', params: { foo: 'bar' } });
 router.resolve({ name: 'user-id', params: { id: 1 } }); // Good
 
 // ---- [foo]-[[bar]].vue
 
 // @ts-expect-error
-router.resolve({ name: 'user-foo-bar' }); // Error
+router.resolve({ name: 'user-foo-bar' });
 // @ts-expect-error
-router.resolve({ name: 'user-foo-bar', params: { bar: 1 } }); // Error
+router.resolve({ name: 'user-foo-bar', params: { bar: 1 } });
 
 router.resolve({ name: 'user-foo-bar', params: { foo: 'bar' } }); // Good
-
 router.resolve({ name: 'user-foo-bar', params: { foo: 'bar', bar: 'baz' } }); // Good
 
 // ---- [...slug].vue
 
 // @ts-expect-error
-router.resolve({ name: 'user-slug' }); // Error
+router.resolve({ name: 'user-slug' });
 // @ts-expect-error
-router.resolve({ name: 'user-slug', params: { slug: 1 } }); // Error
+router.resolve({ name: 'user-slug', params: { slug: 1 } });
 
 router.resolve({ name: 'user-slug', params: { slug: ['foo'] } }); // Good
-
 router.resolve({ name: 'user-slug', params: { slug: [1, 2, 3] } }); // Good
 
 // ---- [one]-foo-[two].vue
 
 // @ts-expect-error
-router.resolve({ name: 'user-one-foo-two' }); // Error
+router.resolve({ name: 'user-one-foo-two' });
 // @ts-expect-error
-router.resolve({ name: 'user-one-foo-two', params: { one: 1 } }); // Error
+router.resolve({ name: 'user-one-foo-two', params: { one: 1 } });
 
 router.resolve({ name: 'user-one-foo-two', params: { one: 1, two: '2' } }); // Good
 
 // ---- [id]/[slug].vue
 
 // @ts-expect-error
-router.resolve({ name: 'user-id-slug' }); // Error
+router.resolve({ name: 'user-id-slug' });
 // @ts-expect-error
-router.resolve({ name: 'user-id-slug', params: { id: 1 } }); // Error
+router.resolve({ name: 'user-id-slug', params: { id: 1 } });
 
 router.resolve({ name: 'user-id-slug', params: { slug: '2' } }); // Good
