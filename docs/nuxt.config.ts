9d4341142e2c51f72c1110cdc1cacd788399e106
Victor Garcia <victor.garcia@malt.com>
2023-02-23 13:56:17 +0100

    Added site verification

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,4 +1,14 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
   css: ['@/assets/theme.css'],
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'google-site-verification',
+          content: 'g3klMOEJCzOJ5ATgQHqYvjYEcdGnpWVC9NpMxrvcqNA',
+        },
+      ],
+    },
+  },
 });
