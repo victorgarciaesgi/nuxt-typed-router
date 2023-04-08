49ca1f7426cf68ce93e2e6619503024f921659dd
Victor Garcia <victorgarciaparis13@gmail.com>
2023-04-08 12:56:58 +0200

    fix: fixed bug when component.d.ts doesn't exists

--- src/core/parser/removeNuxtDefs.ts
+++ src/core/parser/removeNuxtDefs.ts
@@ -1,4 +1,5 @@
 import { readFile } from 'fs/promises';
+import { existsSync } from 'fs';
 import { createResolver } from '@nuxt/kit';
 import { processPathAndWriteFile } from '../fs';
 
@@ -14,44 +15,50 @@ export async function removeNuxtDefinitions({
   const { resolve } = createResolver(import.meta.url);
 
   // Remove NuxtLink from .nuxt/components.d.ts
-  const componentDefinitions = await readFile(resolve(buildDir, 'components.d.ts'), {
-    encoding: 'utf8',
-  });
-  const replacedNuxtLink = componentDefinitions.replace(
-    /'NuxtLink': typeof import\(".*"\)\['default'\]/gm,
-    ''
-  );
+  const componentFilePath = resolve(buildDir, 'components.d.ts');
+  if (existsSync(componentFilePath)) {
+    const componentDefinitions = await readFile(componentFilePath, {
+      encoding: 'utf8',
+    });
+    const replacedNuxtLink = componentDefinitions.replace(
+      /'NuxtLink': typeof import\(".*"\)\['default'\]/gm,
+      ''
+    );
 
-  processPathAndWriteFile({
-    content: replacedNuxtLink,
-    fileName: 'components.d.ts',
-    outDir: '.nuxt',
-  });
+    processPathAndWriteFile({
+      content: replacedNuxtLink,
+      fileName: 'components.d.ts',
+      outDir: '.nuxt',
+    });
+  }
 
   // Remove global imports from .nuxt/types/imports.d.ts
 
   if (autoImport) {
-    let globalDefinitions = await readFile(resolve(buildDir, 'types/imports.d.ts'), {
-      encoding: 'utf8',
-    });
+    const importsFilePath = resolve(buildDir, 'types/imports.d.ts');
+    if (importsFilePath) {
+      let globalDefinitions = await readFile(importsFilePath, {
+        encoding: 'utf8',
+      });
 
-    const importsToRemove = [
-      'useRouter',
-      'useRoute',
-      'useLocalePath',
-      'useLocaleRoute',
-      'definePageMeta',
-      'navigateTo',
-    ].map((m) => new RegExp(`const ${m}: typeof import\\('.*'\\)\\['${m}'\\]`, 'gm'));
+      const importsToRemove = [
+        'useRouter',
+        'useRoute',
+        'useLocalePath',
+        'useLocaleRoute',
+        'definePageMeta',
+        'navigateTo',
+      ].map((m) => new RegExp(`const ${m}: typeof import\\('.*'\\)\\['${m}'\\]`, 'gm'));
 
-    importsToRemove.forEach((imp) => {
-      globalDefinitions = globalDefinitions.replace(imp, '');
-    });
+      importsToRemove.forEach((imp) => {
+        globalDefinitions = globalDefinitions.replace(imp, '');
+      });
 
-    processPathAndWriteFile({
-      content: globalDefinitions,
-      fileName: 'types/imports.d.ts',
-      outDir: '.nuxt',
-    });
+      processPathAndWriteFile({
+        content: globalDefinitions,
+        fileName: 'types/imports.d.ts',
+        outDir: '.nuxt',
+      });
+    }
   }
 }
