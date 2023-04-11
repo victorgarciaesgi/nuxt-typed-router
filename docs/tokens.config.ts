d7132ff1011d7df1be77fb27120c8272a6ea6600
Victor Garcia <victor.garcia@malt.com>
2023-04-11 18:06:30 +0200

    chore: fixed docs rendering

--- docs/tokens.config.ts
+++ docs/tokens.config.ts
@@ -1,7 +1,17 @@
-import { defineTheme, palette } from 'pinceau';
+import { defineTheme } from 'pinceau';
 
 export default defineTheme({
   color: {
-    primary: palette('#0a87db'),
+    primary: {
+      900: '#021b2c',
+      800: '#043353',
+      700: '#064b7a',
+      600: '#0763a1',
+      500: '#0a87db',
+      400: '#2092de',
+      300: '#4ba7e5',
+      200: '#77bceb',
+      100: '#a2d2f1',
+    },
   },
 });
