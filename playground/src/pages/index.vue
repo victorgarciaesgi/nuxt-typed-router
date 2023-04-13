2c5ccf4cc8dd699de1b697b6a3f05313d25dab7b
Victor Garcia <victor.garcia@malt.com>
2023-04-13 16:05:53 +0200

    Updated index file

--- playground/src/pages/index.vue
+++ playground/src/pages/index.vue
@@ -1,63 +1,13 @@
 <template>
-  <div>
-    <button @click="navigate"> Navigate button </button>
-    <nuxt-link :to="{ name: 'admin-id', params: { id: 1 } }">Navigate Link</nuxt-link>
-    <!-- Should error -->
-    <nuxt-link to="/foo" external>Navigate Link</nuxt-link>
-
-    <nuxt-link :to="localePath('/admin/:id')">Navigate Link</nuxt-link>
-    <nuxt-layout></nuxt-layout>
-  </div>
+  <div> Route name: {{ $route.name }} </div>
 </template>
 
 <script setup lang="ts">
 import { TypedRouteLocationRawFromName, helpers, TypedRouteLocation } from '@typed-router';
 
-// definePageMeta({
-//   redirect: (route) => helpers.route({ name: 'admin-id', params: { id: 1 } }),
-// });
-
 definePageMeta({
   name: 'foo-bar',
-  // redirect: { name: 'admin-id', params: { id: 1 } },
 });
 
 const router = useRouter();
-
-const localePath = useLocalePath();
-const localeRoute = useLocaleRoute();
-
-// const route = localeRoute({ name: 'index___en', query: { foo: '1' } });
-// if (route) {
-//   navigateTo(route.fullPath);
-// }
-
-// router.push({ path: '/login' });
-
-function navigate() {
-  const testAssert = '/foo' as TypedRouteLocation;
-  router.push(testAssert);
-  navigateTo(testAssert);
-  router.push({ name: 'user-id-slug', params: { slug: 'bar', id: 1 } });
-  router.push('/user?foo');
-
-  router.push(localePath('/admin/888'));
-
-  const u = 'krzfzlkj' as string;
-  const t = '///';
-
-  const route2 = localePath(`/user/${t}/:slug/articles`); // Should error
-  navigateTo('/foo'); // Should error
-  router.push('/baguette'); // Should error
-
-  const route = localePath(`/user/${u}/:slug/articles`);
-  router.push('/');
-  navigateTo('ednzelfjle', { external: true });
-  router.push({ path: '/' });
-
-  router.push('/user/:id/:slug/articles#baz');
-  router.push('/admin/888'); // ✅
-
-  router.push('/');
-}
 </script>
