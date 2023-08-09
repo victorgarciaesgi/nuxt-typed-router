c8e4bd90c603338f911eb6cc39dacf06ee9639cf
matthieu <matthieu.conti@sky.ch>
2023-08-09 09:49:39 +0200

    routeName for i18n : more firendly names

--- src/core/output/generators/blocks/routes/routes-names-list.block.ts
+++ src/core/output/generators/blocks/routes/routes-names-list.block.ts
@@ -1,7 +1,9 @@
+import { moduleOptionStore } from '../../../../../../src/core/config';
+
 export function createRoutesNamesListExport(routesList: string[]): string {
   return `
   /**
    * Exhaustive list of all the available route names in the app
    * */
-  export type RoutesNamesList = ${routesList.map((m) => `'${m}'`).join('|\n')}`;
+  export type RoutesNamesList = ${routesList.map((m) => `'${moduleOptionStore.i18n ? m.split(moduleOptionStore.i18nOptions?.routesNameSeparator ?? '___')[0] : m}'`).join('|\n')}`;
 }
