3834014fe611494e22e1c3f8cc1e3a4179a2e9ef
Victor Garcia <victor.garcia@malt.com>
2025-12-17 09:52:06 +0100

    chore: analytics

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -9,6 +9,13 @@ export default defineNuxtConfig({
           content: 'g3klMOEJCzOJ5ATgQHqYvjYEcdGnpWVC9NpMxrvcqNA',
         },
       ],
+      script: [
+        {
+          defer: '',
+          src: 'https://cloud.umami.is/script.js',
+          'data-website-id': 'ca4a44f0-dcdb-4b24-83e6-96b5a699235e',
+        },
+      ],
     },
   },
 });
