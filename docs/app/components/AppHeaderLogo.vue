370c893e0025124b3d718388e828f08b1a979622
Victor Garcia <victor.garcia@malt.com>
2026-02-11 10:24:33 +0100

    chore: fixed logo

--- docs/app/components/AppHeaderLogo.vue
+++ docs/app/components/AppHeaderLogo.vue
@@ -4,10 +4,8 @@
 
 <template>
   <NuxtLink to="/" class="flex items-center gap-2.5 shrink-0">
-    <UColorModeImage
-      v-if="appConfig.header?.logo?.dark || appConfig.header?.logo?.light"
-      :light="appConfig.header?.logo?.light || appConfig.header?.logo?.dark"
-      :dark="appConfig.header?.logo?.dark || appConfig.header?.logo?.light"
+    <img
+      src="/logo.png"
       :alt="appConfig.header?.logo?.alt || appConfig.header?.title"
       class="h-7 w-auto shrink-0"
     />
